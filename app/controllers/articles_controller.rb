3b91a6e0ffed814e4ec5798980c011655337b1c7
Marttini Souza <marttini@outlook.com>
2017-11-23 12:54:58 -0200

    Fix create, flash and error messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,9 +6,21 @@ class ArticlesController < ApplicationController
 
   def create
     @articles = Article.new(article_params)
-    @articles.save
-    redirect_to articles_show(@articles)
+    #A opção flash[:notice] usada a seguir é similar as mensagens de sucesso ou erro que aparecem após salvar alguma coisa.
+    if @articles.save 
+      flash[:notice] = "Article was sucessfully created"
+      redirect_to article_path(@articles)
+    else
+      #caso não esteja de acordo com as validações a aplicação ficara na página "new" apresentando as devidas mensagens de erro.
+      render 'new'
+    end
   end
+
+  def show
+    #para apresentar a pagina "show" com as informações do artigo criado e mensagens, é necessários criar a views/articles/show.html.erb
+    @articles = Article.find(params[:id])
+  end
+
 
   private
     def article_params
